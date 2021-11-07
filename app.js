35a981a70e8e7ae6c435d4b2ec6c24bea8e8298e
João Victor Duarte <joaodicu387@gmail.com>
2021-11-07 15:27:25 -0300

    Criando rota privada para obter os dados do usuário

--- app.js
+++ app.js
@@ -15,6 +15,36 @@ app.get("/", (req, res) => {
     res.status(200).json({message: "Bem vindo a nossa API!"});
 });
 
+app.get("/user/:id", checkToken, async(req, res) => {
+    const id = req.params.id;
+
+    const user = await User.findById(id, '-password');
+
+    if(!user) {
+        return res.status(404).json({message: "Usuário não encontrado."});
+    }
+
+    res.status(200).json({user});
+});
+
+function checkToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if(!token) {
+        return res.status(401).json({message: "Acesso negado."});
+    }
+
+    try {
+        const secret = process.env.SECRET;
+        jwt.verify(token, secret);
+        next();
+    }
+    catch (error) {
+        res.status(400).json({message: "Token inválido!"});
+    }
+}
+
 app.post("/auth/register", async(req, res) => {
     const {name, email, password, confirmPassword} = req.body;
 
@@ -63,15 +93,8 @@ app.post("/auth/login", async (req, res) => {
         return res.status(404).json({message: "Usuário não encontrado."});
     }
 
-    console.log(user);
     const userPassword = user.password;
     const checkPassword = await bcrypt.compare(password, userPassword);
-    console.log(checkPassword);
-    console.log(password);
-    console.log(userPassword);
-    console.log(typeof(password));
-    console.log(typeof(userPassword));
-    console.log(password === userPassword);
 
     if(!checkPassword) {
         return res.status(422).json({message: "Senha inválida!"});
